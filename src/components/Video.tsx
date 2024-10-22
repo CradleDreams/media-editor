0ce3c2e27bae0792fe7f18319152870c429d5b36
SEDOYJUNIOUR <zks2003@mail.ru>
2024-10-22 17:35:02 +0300

    fix video ref 2

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -2,26 +2,8 @@ import Konva from "konva";
 import React from "react";
 import {Image} from 'react-konva'
 
-// interface IVideoSetting {
-//   src: string,
-//   ref: any
-// }
 const Video = React.forwardRef((props: any, ref: any) => {
   
-  // const [size, setSize] = React.useState({ width: 200, height: 200 }); 
-
-  // React.useEffect(() => {
-  //   const onload = function() {
-  //     setSize({
-  //       width: ref.current.videoWidth,
-  //       height: ref.current.videoHeight
-  //     });
-  //   };
-  //   ref.current.addEventListener("loadedmetadata", onload);
-  //   return () => {
-  //     ref.current.removeEventListener("loadedmetadata", onload);
-  //   };
-  // }, [ref.current]);
   React.useEffect(() => {
     const layer = ref.current.getLayer();
 
@@ -35,12 +17,10 @@ const Video = React.forwardRef((props: any, ref: any) => {
     <Image
         ref={ref}
         image={ref.current}
-        x={20}
-        y={20}
-        stroke="red"
+        stroke="black"
         width={700}
         height={400}
-        draggable
+
       />
   );
 });
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,14 @@
-import { Layer, Stage} from "react-konva";
+import { Layer, Rect, Stage } from "react-konva";
 import { useState } from "react";
 import React from "react";
 import ButtonVideo from "../shared/ui/ButtonVideo";
 import Video from "../components/Video";
 
+
 function App() {
   const [src, setSrc] = useState<any>();
   const imageRef = React.useRef<any>();
 
-  
   const handleOnChange = (e: React.FormEvent<HTMLInputElement>) => {
     const target = e.target as HTMLInputElement & {
       files: FileList;
@@ -18,35 +18,34 @@ function App() {
     file.onload = function () {
       setSrc(file.result);
     };
-
-    file.readAsDataURL(target.files[0]);
-    
+ 
+    file.readAsDataURL(target.files[0]);   
   };
 
   React.useMemo(() => {
     const element = document.createElement("video");
     element.src = src;
-    imageRef.current = element
+    imageRef.current = element;
     return element;
   }, [src, imageRef]);
 
-  
-
   const handlePlay = () => {
-    imageRef.current.getImage().play();
+    imageRef.current.play();
   };
 
   const handlePause = () => {
-    imageRef.current.getImage().pause();
+    imageRef.current.pause();
   };
 
   return (
     <>
-      <Stage width={window.outerWidth} height={500}>
-        <Layer>
-          {src && (<Video ref={imageRef}/>)}
+      <Stage width={window.innerWidth} height={400} visible={true}>
+        <Layer width={700} height={400} x={window.innerWidth/4} draggable>
+        <Rect width={700} height={400} fill={"black"}/>
+          <Video ref={imageRef} />
         </Layer>
       </Stage>
+
       <input type={"file"} accept={"video/mp4"} onChange={handleOnChange} />
       <ButtonVideo text={"Play"} func={handlePlay} />
       <ButtonVideo text={"Pause"} func={handlePause} />
